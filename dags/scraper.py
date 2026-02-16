f8bd8cc39509bb79d25efba215955844b36d8acd
Rashid Hajiyev <rashid.h2001@gmail.com>
2026-02-16 17:47:16 +0400

    work and loads eng, rus, az and turkish books

--- dags/scraper.py
+++ dags/scraper.py
@@ -4,7 +4,7 @@ import pandas as pd
 import time
 from sqlalchemy import create_engine, text
 
-def scrape_page(page: int):
+def scrape_page_eng(page: int):
     if page == 1:
         url = "https://alinino.az/collection/knigi-na-angliyskom-yazyke"
     else:
@@ -45,11 +45,139 @@ def scrape_page(page: int):
     return pd.DataFrame(data)
 
 
-def scrape_all_pages(start=1, end=412):
+
+def scrape_page_az(page: int):
+    if page == 1:
+        url = "https://alinino.az/collection/knigi-na-azerbaydzhanskom-yazyke"
+    else:
+        url = f"https://alinino.az/collection/knigi-na-azerbaydzhanskom-yazyke?page={page}"
+
+    response = rq.get(url, timeout=10)
+    response.raise_for_status()
+
+    soup = bs(response.text, "html.parser")
+
+    names = soup.find_all("a", class_="product-card__title")
+    prices = soup.find_all("span", class_="product-card__price")
+    # old_prices = soup.find_all("span", class_="product-card__old-price")
+
+    data = []
+
+    for i in range(len(names)):
+        name = names[i].text.strip()
+
+        price_parts = prices[i].text.strip().split()
+        current_price = price_parts[0]
+        currency = price_parts[1] if len(price_parts) > 1 else None
+
+        # # Old price may be missing
+        # old_price = (
+        #     old_prices[i].text.strip().split()[0]
+        #     if i < len(old_prices)
+        #     else None
+        # )
+
+        data.append({
+            "Product Name": name,
+            "Current Price": current_price,
+            #"Old Price": old_price,
+            "Currency": currency
+        })
+
+    return pd.DataFrame(data)
+
+
+
+def scrape_page_rus(page: int):
+    if page == 1:
+        url = "https://alinino.az/collection/knigi-na-russkom-yazyke"
+    else:
+        url = f"https://alinino.az/collection/knigi-na-russkom-yazyke?page={page}"
+
+    response = rq.get(url, timeout=10)
+    response.raise_for_status()
+
+    soup = bs(response.text, "html.parser")
+
+    names = soup.find_all("a", class_="product-card__title")
+    prices = soup.find_all("span", class_="product-card__price")
+    # old_prices = soup.find_all("span", class_="product-card__old-price")
+
+    data = []
+
+    for i in range(len(names)):
+        name = names[i].text.strip()
+
+        price_parts = prices[i].text.strip().split()
+        current_price = price_parts[0]
+        currency = price_parts[1] if len(price_parts) > 1 else None
+
+        # # Old price may be missing
+        # old_price = (
+        #     old_prices[i].text.strip().split()[0]
+        #     if i < len(old_prices)
+        #     else None
+        # )
+
+        data.append({
+            "Product Name": name,
+            "Current Price": current_price,
+            #"Old Price": old_price,
+            "Currency": currency
+        })
+
+    return pd.DataFrame(data)
+
+
+
+def scrape_page_turk(page: int):
+    if page == 1:
+        url = "https://alinino.az/collection/knigi-na-turetskom-yazyke"
+    else:
+        url = f"https://alinino.az/collection/knigi-na-turetskom-yazyke?page={page}"
+
+    response = rq.get(url, timeout=10)
+    response.raise_for_status()
+
+    soup = bs(response.text, "html.parser")
+
+    names = soup.find_all("a", class_="product-card__title")
+    prices = soup.find_all("span", class_="product-card__price")
+    # old_prices = soup.find_all("span", class_="product-card__old-price")
+
+    data = []
+
+    for i in range(len(names)):
+        name = names[i].text.strip()
+
+        price_parts = prices[i].text.strip().split()
+        current_price = price_parts[0]
+        currency = price_parts[1] if len(price_parts) > 1 else None
+
+        # # Old price may be missing
+        # old_price = (
+        #     old_prices[i].text.strip().split()[0]
+        #     if i < len(old_prices)
+        #     else None
+        # )
+
+        data.append({
+            "Product Name": name,
+            "Current Price": current_price,
+            #"Old Price": old_price,
+            "Currency": currency
+        })
+
+    return pd.DataFrame(data)
+
+
+
+
+def scrape_all_pages_eng(start=1, end=412):
     all_data = []
 
     for page in range(start, end + 1):
-        df = scrape_page(page)
+        df = scrape_page_eng(page)
         all_data.append(df)
 
         time.sleep(3)  # break for a second
@@ -57,6 +185,52 @@ def scrape_all_pages(start=1, end=412):
     final_df = pd.concat(all_data, ignore_index=True)
     final_df.index += 1
     return final_df
+
+
+
+def scrape_all_pages_az(start=1, end=145):
+    all_data = []
+
+    for page in range(start, end + 1):
+        df = scrape_page_az(page)
+        all_data.append(df)
+
+        time.sleep(3)  # break for a second
+
+    final_df = pd.concat(all_data, ignore_index=True)
+    final_df.index += 1
+    return final_df
+
+
+
+def scrape_all_pages_rus(start=1, end=712):
+    all_data = []
+
+    for page in range(start, end + 1):
+        df = scrape_page_rus(page)
+        all_data.append(df)
+
+        time.sleep(3)  # break for a second
+
+    final_df = pd.concat(all_data, ignore_index=True)
+    final_df.index += 1
+    return final_df
+
+
+
+def scrape_all_pages_turk(start=1, end=190):
+    all_data = []
+
+    for page in range(start, end + 1):
+        df = scrape_page_turk(page)
+        all_data.append(df)
+
+        time.sleep(3)  # break for a second
+
+    final_df = pd.concat(all_data, ignore_index=True)
+    final_df.index += 1
+    return final_df
+
 
 
 
--- dags/scraper_dag.py
+++ dags/scraper_dag.py
@@ -1,31 +1,80 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
+from airflow.sensors.time_delta import TimeDeltaSensor
 from datetime import datetime, timedelta
 import pytz
-from scraper import scrape_all_pages, upload_to_postgres
+
+from scraper import (
+    scrape_all_pages_eng,
+    scrape_all_pages_az,
+    scrape_all_pages_rus,
+    scrape_all_pages_turk,
+    upload_to_postgres
+)
+
+# timezone
+baku_tz = pytz.timezone("Asia/Baku")
 
 
-def etl_task():
-    # Scrape data from all pages
-    df = scrape_all_pages(start=1, end=412)
 
-    # Upload data to PostgreSQL
+def scrape_eng():
+    df = scrape_all_pages_eng(start=1, end=412)
     upload_to_postgres(
         df=df,
         host='postgres',
         database='books_db',
-        table_name='books',
+        table_name='books_eng',
         username='books_user',
         password='books_password',
         port=5432
     )
 
-baku_tz = pytz.timezone("Asia/Baku")
+
+def scrape_az():
+    df = scrape_all_pages_az(start=1, end=145)
+    upload_to_postgres(
+        df=df,
+        host='postgres',
+        database='books_db',
+        table_name='books_az',
+        username='books_user',
+        password='books_password',
+        port=5432
+    )
+
+
+def scrape_rus():
+    df = scrape_all_pages_rus(start=1, end=712)
+    upload_to_postgres(
+        df=df,
+        host='postgres',
+        database='books_db',
+        table_name='books_rus',
+        username='books_user',
+        password='books_password',
+        port=5432
+    )
+
+
+def scrape_turk():
+    df = scrape_all_pages_turk(start=1, end=190)
+    upload_to_postgres(
+        df=df,
+        host='postgres',
+        database='books_db',
+        table_name='books_turk',
+        username='books_user',
+        password='books_password',
+        port=5432
+    )
+
+
+# ---------- DAG ----------
 
 with DAG(
     dag_id="alinino_scraper_dag",
     start_date=datetime(2026, 2, 5, tzinfo=baku_tz),
-    schedule_interval="0 6 * * *",  # 6:00 AM Baku time
+    schedule_interval="0 6 * * *",
     catchup=False,
     default_args={
         "retries": 2,
@@ -34,7 +83,42 @@ with DAG(
     tags=["scraper", "alinino"]
 ) as dag:
 
-    run_etl = PythonOperator(
-        task_id="scrape_and_upload",
-        python_callable=etl_task
+    task_eng = PythonOperator(
+        task_id="scrape_eng",
+        python_callable=scrape_eng
     )
+
+    wait_1 = TimeDeltaSensor(
+        task_id="wait_30min_after_eng",
+        delta=timedelta(minutes=30)
+    )
+
+    task_az = PythonOperator(
+        task_id="scrape_az",
+        python_callable=scrape_az
+    )
+
+    wait_2 = TimeDeltaSensor(
+        task_id="wait_30min_after_az",
+        delta=timedelta(minutes=30)
+    )
+
+    task_rus = PythonOperator(
+        task_id="scrape_rus",
+        python_callable=scrape_rus
+    )
+
+    wait_3 = TimeDeltaSensor(
+        task_id="wait_30min_after_rus",
+        delta=timedelta(minutes=30)
+    )
+
+    task_turk = PythonOperator(
+        task_id="scrape_turk",
+        python_callable=scrape_turk
+    )
+
+
+    # ---------- PIPELINE ORDER ----------
+
+    task_eng >> wait_1 >> task_az >> wait_2 >> task_rus >> wait_3 >> task_turk
